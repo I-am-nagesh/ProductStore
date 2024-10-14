5625e96efc1fb47191bb7b21d162db6b62afc7f0
I-am-nagesh <i.am.nageshpandey@gmail.com>
2024-10-14 17:39:19 +0530

    added get post put delete HTTP Method

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,23 @@ import express from "express";
 import "dotenv/config";
 import connectDB from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 const app = express();
 connectDB();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
+
+//GET route to retrieve product
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "server Error" });
+  }
+});
 
 // POST route to create a product
 app.post("/api/products", async (req, res) => {
@@ -25,6 +37,26 @@ app.post("/api/products", async (req, res) => {
     res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
     console.error("Error in create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// PUT riute to update a product by ID
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ succes: true, data: updatedProduct });
+  } catch (error) {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 });
